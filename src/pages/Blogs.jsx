8d51bb61e8a4daf4e074ce985892049a96233561
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 00:15:03 +0800

    Blog page start

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import BlogsPage from '../components/BlogsPage'
 
 const Blogs = () => {
   return (
     <div>
       <div className='py-40 bg-black text-center text-white px-4'>
         <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Blog Page</h1>
+      </div>
+
+      {/* all blogs container */}
+      <div className='max-w-7xl mx-auto'>
+        <BlogsPage />
       </div>
     </div>
   )
