d34438bf1a9f379587e036efb278031257b6570f
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 00:07:36 +0800

    Every page banner design

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='py-40 bg-black text-center text-white px-4'>
+        <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Contact Page</h1>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Server = () => {
   return (
-    <div>Server</div>
+    <div>
+      <div className='py-40 bg-black text-center text-white px-4'>
+        <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Services Page</h1>
+      </div>
+    </div>
   )
 }
 
