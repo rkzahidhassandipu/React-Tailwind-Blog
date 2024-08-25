bc13416a053a22e827bf8a11cb9044b0cc3b4cc3
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 22:10:00 +0800

    Modal section password input adde

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,10 +8,15 @@ const Modal = ({isOpen, onClose}) => {
                 <h2 className='text-xl font-extrabold mb-4 mt-6 uppercase'>Please Login Here!</h2>
                 <form action="" className='px-4'>
                     <div className='mb-5'>
-                        <input type="email" name='email' id='email' placeholder='Example@gmail.com' className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-4 mx-6 text-base font-medium text-[#6b7280]' />
+                        <input type="email" name='email' id='email' placeholder='Example@gmail.com' className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-4 text-base font-medium text-[#6b7280]' />
                     </div>
                     <div className='mb-5'>
-                        <input type="password" name='password' id='password' placeholder='Enter yor password' className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-4 mx-6 text-base font-medium text-[#6b7280]' />
+                        <input type="password" name='password' id='password' placeholder='Enter yor password' className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-4 text-base font-medium text-[#6b7280]' />
+                    </div>
+
+                    <div>
+                        <button className='mr-5 hover:shadow-md rounded-md bg-[#6a64f1] hover:bg-orange-600 py-3 px-8 text-base font-semibold text-white outline-none'>Login</button>
+                        <button className='ml-5 bg-gray-300 hover:bg-gray-400 duration-300 text-gray-800 font-semibold py-3 px-8 rounded inline-flex items-center mt-5'>Close</button>
                     </div>
                 </form>
             </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ContactForm from '../components/Contactpage'
 
 const Contact = () => {
   return (
@@ -6,6 +7,7 @@ const Contact = () => {
       <div className='py-40 bg-black text-center text-white px-4'>
         <h1 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>Contact Page</h1>
       </div>
+      <ContactForm />
     </div>
   )
 }
