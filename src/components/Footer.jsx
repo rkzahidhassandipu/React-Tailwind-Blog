a7841441b750219ba2ae4b3fd07970c8630150f7
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 19:20:37 +0800

    Footer section Design start

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-gray-900'>
+        <div className='px-4'>
+
+        </div>
+    </div>
   )
 }
 
