c15835faf293119247ef83d66d4d5218d9409d9e
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 19:30:53 +0800

    Footer left section desing complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,24 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-gray-900'>
-        <div className='px-4'>
+        <div className='px-16 pt-16 pb-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-4'>
             <div>
-                <div>
+                <div className='grid grid-cols-2 gap-5 lg:col-span-4 md:grid-cols-4'>
                     <div>
-                        <p></p>
-                        <ul>
-                            <li>News</li>
-                            <li>World</li>
-                            <li>Games</li>
-                            <li>References</li>
+                        <p className='font-medium -tracking-wide text-gray-300'>Category</p>
+                        <ul className='mt-2 space-y-2'>
+                            <li>
+                                <a className='text-gray-500 transition-colors duration-500 hover:text-orange-500' href="">News</a>
+                            </li>
+                            <li>
+                                <a className='text-gray-500 transition-colors duration-500 hover:text-orange-500' href="">World</a>
+                            </li>
+                            <li>
+                                <a className='text-gray-500 transition-colors duration-500 hover:text-orange-500' href="">Games</a>
+                            </li>
+                            <li>
+                                <a className='text-gray-500 transition-colors duration-500 hover:text-orange-500' href="">References</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
