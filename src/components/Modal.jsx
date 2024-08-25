c60b75ca94ca98f95a98316f94cb5f2a7c85deee
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 21:51:36 +0800

    Modal section email input added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = ({isOpen, onClose}) => {
   return (
-    <div>Modal</div>
+    <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${isOpen ? "" : "hidden"}`}>
+        <div className='modal-container'>
+            <div className='bg-indigo-700 text-center p-5  h-96 lg:w-[500px] rounded shadow-md'>
+                <h2 className='text-xl font-extrabold mb-4 mt-6 uppercase'>Please Login Here!</h2>
+                <form action="" className='px-4'>
+                    <div>
+                        <input type="email" name='email' id='email' placeholder='Example@gmail.com' className='w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-4 mx-6 text-base font-medium text-[#6b7280]' />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
