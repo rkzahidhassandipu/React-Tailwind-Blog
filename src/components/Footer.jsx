d46197bf249af9a5b501c7a1bd1143709b18c9af
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 19:23:18 +0800

    Footer left section items adding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,19 @@ const Footer = () => {
   return (
     <div className='bg-gray-900'>
         <div className='px-4'>
-
+            <div>
+                <div>
+                    <div>
+                        <p></p>
+                        <ul>
+                            <li>News</li>
+                            <li>World</li>
+                            <li>Games</li>
+                            <li>References</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
