7d84980b8ea8e8ed7bae44cb36afaf83285f1aea
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 15:05:36 +0800

    Blog page sidebar popular blogs

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 
 const SideBar = () => {
     const [popularBlogs, setPopularBlogs] = useState([]);
@@ -33,16 +34,30 @@ const SideBar = () => {
     }
   return (
     <div>
-    <h3 className='text-2xl font-semibold px-4'>Latest Blogs</h3>
-    <div >
-        {popularBlogs.slice(0, 5).map(blog => (
-            <div className='my-5 border-b-2 border-spacing-2 px-4' key={blog.id}>
-                <h4 className='font-medium mb-2'>{blog.title}</h4>
-                <Link to='/' className='text-base pb-2 font-medium hover:text-orange-500 inline-flex items-center py-1'>Read more</Link>
+        <div>
+            <h3 className='text-2xl font-semibold px-4'>Latest Blogs</h3>
+            <div >
+                {popularBlogs.slice(0, 5).map(blog => (
+                    <div className='my-5 border-b-2 border-spacing-2 px-4' key={blog.id}>
+                        <h4 className='font-medium mb-2'>{blog.title}</h4>
+                        <Link to='/' className='text-base pb-2 font-medium hover:text-orange-500 inline-flex items-center py-1'>Read more <FaArrowRight className='mt-1 ml-2' /></Link>
+                    </div>
+                ))}
             </div>
-        ))}
+        </div>
+        {/* Popular blog */}
+        <div>
+            <h3 className='text-2xl font-semibold px-4 mt-20'>Popular Blogs</h3>
+            <div >
+                {popularBlogs.slice(6, 10).map(blog => (
+                    <div className='my-5 border-b-2 border-spacing-2 px-4' key={blog.id}>
+                        <h4 className='font-medium mb-2'>{blog.title}</h4>
+                        <Link to='/' className='text-base pb-2 font-medium hover:text-orange-500 inline-flex items-center py-1'>Read more <FaArrowRight className='mt-1 ml-2' /></Link>
+                    </div>
+                ))}
+            </div>
+        </div>
     </div>
-</div>
   )
 }
 
