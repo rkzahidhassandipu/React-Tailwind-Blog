fbc416b1c51459c9048848650885d6792601532f
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 19:17:32 +0800

    Home page blog section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Banner from '../components/Banner'
+import Banner from '../components/Banner';
+import BlogsPage from '../components/BlogsPage';
 
 const Home = () => {
   return (
     <div>
       <Banner />
 
-      
+      <div className='max-w-7xl mx-auto'>
+        <BlogsPage />
+      </div>
+
     </div>
   )
 }
