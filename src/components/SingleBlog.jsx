e52307406ec403e36236b4d79e8b1cb6cc25a6cd
Zahid <rkrazzkhan01731@gmail.com>
2024-08-25 21:24:47 +0800

    singl blog page section dynamic info complete

--- src/components/SingleBlog.jsx
+++ src/components/SingleBlog.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom';
+import { FaClock, FaUser } from 'react-icons/fa6';
 
 const SingleBlog = () => {
     const data = useLoaderData();
@@ -9,6 +10,26 @@ const SingleBlog = () => {
     <div>
         <div>
             <h2>Single Blog Page</h2>
+        </div>
+
+        <div className='max-w-7xl mx-auto my-12'>
+            <div className='lg:w-3/4 mx-auto'>
+                <div>
+                    <img src={image} alt="" className='w-full mx-auto rounded' />
+                </div>
+                <h2 className='text-3xl mt-8 font-bold mb-4 text-blue-500 cursor-pointer'>{title}</h2>
+                <p className='mb-3 text-gray-600'>
+                    <FaUser className='inline-flex items-center mr-2' />
+                    {author} | {published_date}
+                </p>
+                <p className='mb-3 text-gray-600'>
+                    <FaClock className='inline-flex items-center mr-2' />
+                    {reading_time}
+                </p>
+                <p className='mb-3 text-gray-600 text-base'>
+                    {content}
+                </p>
+            </div>
         </div>
     </div>
   )
